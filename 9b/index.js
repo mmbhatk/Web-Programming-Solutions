9bbab9b42b656107851835547b6b6e56a466ff2e
mmbhatk <kmanasvibhat@gmail.com>
2019-05-25 20:26:10 +0530

    Added 9b.

---
+++ 9b/index.js
@@ -0,0 +1,44 @@
+var express = require('express')
+var app = express()
+var MongoClient = require('mongodb').MongoClient
+var url = "mongodb://127.0.0.1/student"
+
+MongoClient.connect(url, function(err, db) {
+	if(!err)
+	{
+		console.log("Connected to the database.")
+
+		app.get('/', function(req, res) {
+			res.sendFile(__dirname + '/form.html')
+		})
+
+		app.get('/insert', function(req, res) {
+			db.collection('student').insert({name: req.query.uname, branch: req.query.branch, sem: req.query.sem})
+			console.log("Inserted:\t" + JSON.stringify(req.query))
+			res.end("Inserted:\t" + JSON.stringify(req.query))
+		})
+
+		app.get('/display', function(req, res) {
+			db.collection('student').find({}, {_id: 0}).toArray(function(err, data) {
+				console.log(JSON.stringify(data))
+				res.end(JSON.stringify(data))
+			})
+		})
+
+		app.get('/cse6', function(req, res) {
+			db.collection('student').find({branch: 'cse', sem: '6'}, {_id: 0, branch: 0, sem: 0}).toArray(function(err, data) {
+				console.log(JSON.stringify(data))
+				res.end(JSON.stringify(data))
+			})
+		})
+
+		app.get('/drop', function(req, res) {
+			db.collection('student').drop()
+			console.log('Collection has been dropped.')
+			res.end('Collection has been dropped.')
+		})
+
+		app.listen(5000)
+	}
+	else console.log("Error.")
+})
