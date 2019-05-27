7fc2e7848c34a00bfc58fa81d7b0793090f1fd8c
mmbhatk <kmanasvibhat@gmail.com>
2019-05-27 18:02:11 +0530

    Made a few changes.

--- 8a/index.js
+++ 8a/index.js
@@ -1,15 +1,12 @@
-function vowelCount(s) {
+function vowelCount(s)
+{
+	s = s.split("")
 	var vowels = {a: 0, e: 0, i: 0, o: 0, u: 0}
-	s = s.split('')
-	for(var i = 0; i < s.length; i++) {
-		if(s[i] == 'a') vowels.a += 1
-		else if(s[i] == 'e') vowels.e += 1
-		else if(s[i] == 'i') vowels.i += 1
-		else if(s[i] == 'o') vowels.o += 1
-		else if(s[i] == 'u') vowels.u += 1
-	}
+	for(var i = 0; i < s.length; i++)
+		if(s[i] in vowels)
+			vowels[s[i]] += 1
 	return vowels
 }
 
-var s = "Le Tour de France"
-console.log(vowelCount(s))
+var vowels = vowelCount('Le Tour de France')
+for(v in vowels) console.log("Number of times " + v + " occurs: " + vowels[v])
